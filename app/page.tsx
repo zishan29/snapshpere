7c248fc760dabef8a6e781bd6ee28ff12fc53f92
Zishan Shaikh <100702557+zishan29@users.noreply.github.com>
2024-01-30 19:43:27 +0530

    Remove token verification

--- app/page.tsx
+++ app/page.tsx
@@ -8,30 +8,37 @@ import HomePage from "./components/HomePage";
 export default function Home() {
   const router = useRouter();
 
-  useEffect(() => {
-    (async () => {
-      try {
-        let res = await fetch("https://snap-talk.adaptable.app/verifyToken", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ token: localStorage.getItem("token") }),
-        });
-        if (res.status === 401) {
-          let resData = await res.json();
-          console.log(resData.error);
-          router.push("/login");
-        }
-      } catch (err) {
-        console.log(err);
-      }
-    })();
-  });
+  if (typeof window !== "undefined") {
+    if (!localStorage.getItem("token")) {
+      router.push("/login");
+    }
+  }
+
+  // useEffect(() => {
+  //   (async () => {
+  //     try {
+  //       let res = await fetch("https://snap-talk.adaptable.app/verifyToken", {
+  //         method: "POST",
+  //         headers: {
+  //           "Content-Type": "application/json",
+  //         },
+  //         body: JSON.stringify({ token: localStorage.getItem("token") }),
+  //       });
+  //       if (res.status === 401) {
+  //         let resData = await res.json();
+  //         console.log(resData.error);
+  //         router.push("/login");
+  //       }
+  //     } catch (err) {
+  //       console.log(err);
+  //     }
+  //   })();
+  // });
+
   return (
     <main className="flex min-h-screen flex-col items-center bg-gray-900 text-white">
       <Nav />
-      <div className="sm:w-8/12 lg:w-6/12 xl:w-4/12  flex flex-col items-center h-full grow gap-0">
+      <div className="sm:w-8/12 lg:w-6/12 xl:w-4/12 flex flex-col items-center h-full grow gap-0">
         <HomePage />
       </div>
     </main>
